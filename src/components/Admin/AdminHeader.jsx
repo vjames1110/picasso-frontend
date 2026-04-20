61ccc7e70102533e66ad8ab585c0a3dfa1d2aadf
vjames1110 <vikashjames8@gmail.com>
2026-04-20 17:22:52 +0530

    Modified Footer and Admin Header

--- src/components/Admin/AdminHeader.jsx
+++ src/components/Admin/AdminHeader.jsx
@@ -9,7 +9,7 @@ const AdminHeader = ({ toggleSidebar }) => {
 
   const handleLogout = () => {
     logout();
-    navigate("/admin/login");
+    navigate("/");
   };
 
   return (
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import "./Footer.css";
 
 const Footer = () => {
@@ -9,22 +10,56 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
 
+        {/* LEFT */}
         <div className="footer-left">
           <h3>Picasso Publications</h3>
-          <p>Your trusted ebook store</p>
+          <p>All Rights Reserved</p>
         </div>
 
+
+        {/* CENTER */}
         <div className="footer-center">
-          <p>© 2026 Picasso Publications</p>
+          <a
+            href="https://smartsightanalytics.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="developer-link"
+          >
+            @ Developed by Smart Sight Analytics, Bilaspur
+          </a>
         </div>
 
+
+        {/* RIGHT */}
         <div className="footer-right">
+
+          <div className="social-icons">
+
+            <a
+              href="https://www.facebook.com/picasso.bilaspur"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebookF />
+            </a>
+
+            <a
+              href="https://www.instagram.com/picasso_publication_cg?igsh=bG9zMTBvZWYzNGZp"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram />
+            </a>
+
+          </div>
+
           <span
             className="admin-link"
             onClick={() => navigate("/admin/login")}
           >
             Admin Login
           </span>
+
         </div>
 
       </div>
