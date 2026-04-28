7edb12500f4535b8ade2168f3b0911bdfc147d33
vjames1110 <vikashjames8@gmail.com>
2026-04-28 14:28:59 +0530

    Added timeout

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 
-  timeout: 10000, // 10 seconds timeout
+  timeout: 30000, // 30 seconds timeout
 });
 
 // Attach token automatically if exists
